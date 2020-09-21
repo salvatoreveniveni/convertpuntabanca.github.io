f08278da1fe40bbd61efaaf47f3d0928e7f7b0f8
salvatoreveniveni <36447214+salvatoreveniveni@users.noreply.github.com>
2020-09-21 12:31:07 +0200

    first commit

---
+++ precache-manifest.d7519000313ce708c056abee9f97e20e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56aa05e14fd9556fdd953f16b83df9a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59cb4468ed21d9c34b83",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ed94a50b247434bbcd07",
+    "url": "/static/css/main.fccd9fe4.chunk.css"
+  },
+  {
+    "revision": "59cb4468ed21d9c34b83",
+    "url": "/static/js/2.82dc78ec.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.82dc78ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed94a50b247434bbcd07",
+    "url": "/static/js/main.202a48a2.chunk.js"
+  },
+  {
+    "revision": "969c8716530dc2fb8265",
+    "url": "/static/js/runtime-main.8364b0e9.js"
+  }
+]);
