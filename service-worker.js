7db02abb4b4a9718221d8b9151ba5e979081da9b
salvatoreveniveni <36447214+salvatoreveniveni@users.noreply.github.com>
2020-09-21 15:10:49 +0200

    5 commit add button

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6b51eb388451ed5d7358d739d201a760.js"
+  "/precache-manifest.9c214892c06fda1eba83f4ea67fdcc0b.js"
 );
 
 self.addEventListener('message', (event) => {
